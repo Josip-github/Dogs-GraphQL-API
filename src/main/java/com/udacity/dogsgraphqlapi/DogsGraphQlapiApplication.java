348622e05047b5492f930c4236847ce0716be1f8
Josip-github <josipcota1@gmail.com>
2021-11-29 10:47:01 +0100

    Initial commit Starter dependencies: Spring web, H2, Spring Data JPA Manually added dependencies: graphql, graphql tools, graphiql Configurations concerning H2 database and graphql are set in application.properties File data.sql serves for populating the database table dog with some data.

---
+++ src/main/java/com/udacity/dogsgraphqlapi/DogsGraphQlapiApplication.java
@@ -0,0 +1,13 @@
+package com.udacity.dogsgraphqlapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogsGraphQlapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogsGraphQlapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/udacity/dogsgraphqlapi/DogsGraphQlapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.dogsgraphqlapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogsGraphQlapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
